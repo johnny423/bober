e2ab798e104b2d1654b471ac2ee23c9748e22373
Eli Alkhazov <eli.alkhazov@gmail.com>
2024-07-22 21:37:46 +0300

    open new file window from main window + add close and exit btns

---
+++ bober/src/fe/windows/load_file_window.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+
+class LoadFileWindow(tk.Toplevel):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.title("Load File")
+        self.geometry("300x200")  # Set a default size  todo check if redundant
+
+        # Add some content to the window
+        label = tk.Label(self, text="This is the Load File window")
+        label.pack(pady=20)
+
+        # Add a close button
+        close_button = tk.Button(self, text="Close", command=self.destroy)
+        # close_button.pack(pady=10)
+
--- bober/src/fe/windows/main_window.py
+++ bober/src/fe/windows/main_window.py
@@ -1,14 +1,23 @@
 import tkinter as tk
-from . import utils
+
+from bober.src.fe.windows import utils
+from bober.src.fe.windows.load_file_window import LoadFileWindow
 
 
 class MainWindow(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Main Window")
-        utils.add_button(self, "Load new file", utils.dummy_button_command, 0, 0)
+        utils.add_button(self, "Load new file", self.open_load_file_window, 0, 0)
         utils.add_button(self, "Find document", utils.dummy_button_command, 1, 0)
         utils.add_button(self, "Word index", utils.dummy_button_command, 2, 0)
         utils.add_button(self, "Find word by index", utils.dummy_button_command, 3, 0)
         utils.add_button(self, "Manage word groups", utils.dummy_button_command, 4, 0)
         utils.add_button(self, "Manage linguistic expressions", utils.dummy_button_command, 5, 0)
+
+        # Add a close button
+        utils.add_button(self, "Exit", self.destroy, 6, 0)
+        # close_button.pack(pady=10)
+
+    def open_load_file_window(self):
+        LoadFileWindow(self)
