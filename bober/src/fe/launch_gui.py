1b06e810b4ff43b518d4c4d366a0780643bdae89
Eli Alkhazov <eli.alkhazov@gmail.com>
2024-07-23 00:42:06 +0300

    move main window to different file

--- bober/src/fe/launch_gui.py
+++ bober/src/fe/launch_gui.py
@@ -1,4 +1,27 @@
-from bober.src.fe.windows.main_window import MainWindow
+import tkinter as tk
+
+from bober.src.loader import load_single_file
+from bober.src.fe.windows import utils
+from bober.src.fe.windows.load_file_window import LoadFileWindow
+
+
+class MainWindow(tk.Tk):
+    def __init__(self, session):
+        super().__init__()
+        self.session = session
+        self.title("Main Window")
+        utils.create_button(self, "Load new file", self.open_load_file_window)
+        utils.create_button(self, "Find document", utils.dummy_button_command)  # todo
+        utils.create_button(self, "Word index", utils.dummy_button_command)  # todo
+        utils.create_button(self, "Find word by index", utils.dummy_button_command)  # todo
+        utils.create_button(self, "Manage word groups", utils.dummy_button_command)  # todo
+        utils.create_button(self, "Manage linguistic expressions", utils.dummy_button_command)  # todo
+        utils.create_button(self, "Exit", self.destroy)
+
+    def open_load_file_window(self):
+        def load_file_callback(*args):
+            return load_single_file(self.session, *args)
+        LoadFileWindow(self, load_file_callback)
 
 
 def launch_gui(session):
@@ -7,4 +30,4 @@ def launch_gui(session):
 
 
 if __name__ == "__main__":
-    launch_gui(None)  # can raise an error downstream
+    launch_gui(None)  # can raise an error downstream, only for dev/debug
